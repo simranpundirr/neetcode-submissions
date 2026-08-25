367db684d9db689354b82164fb94979897aec791
Simran Pundir <122039675+simranpundirr@users.noreply.github.com>
2026-08-26 01:00:40 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string ans="";
+        for(string s:strs){
+            ans+=to_string(s.length());
+            ans+="#";
+            ans+=s;
+        }
+        return ans;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j]!='#'){
+                j++;
+            }
+            int len=stoi(s.substr(i,j-i));
+            string word=s.substr(j+1,len);
+            ans.push_back(word);
+            i=j+len+1;
+        }
+        return ans;
+    }
+};
