14da233ca8db4af05cf0e165d05796eddc4478a4
Simran Pundir <122039675+simranpundirr@users.noreply.github.com>
2026-08-29 13:32:24 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int n=numbers.size();
+        int i=0;
+        int j=n-1;
+        vector<int> sol;
+        while(i<j){
+            if(numbers[i]+numbers[j]==target){
+                sol.push_back(i+1);
+                sol.push_back(j+1);
+                return sol;
+            }
+            else if(numbers[i]+numbers[j]<target){
+                i++;
+            }
+            else{
+                j--;
+            }
+
+        }
+        return sol;
+    }
+};
