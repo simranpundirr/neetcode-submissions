fddd077efc1674f13a0041f56a4fb43ecb052aef
Simran Pundir <122039675+simranpundirr@users.noreply.github.com>
2026-09-04 02:23:19 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       int prof=0;
+       for(int i=0;i<prices.size()-1;i++){
+        if(prices[i+1]>prices[i]){
+            prof+=prices[i+1]-prices[i];
+            
+        }
+        else{
+            continue;
+        }
+       } 
+       return prof;
+    }
+};
